d20fcebc3399287f619e9b8b61472943f9f6321a
Ye <fusen1026@163.com>
2022-06-26 21:54:58 +0800

    fixed

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import axios from 'axios'
 // 替换 baseURL
 axios.defaults.baseURL = 'http://apis.imooc.com/api/'
 axios.interceptors.request.use(config => {
-  config.params = { ...config.params, icode: '24203399705CE782' }
+  config.params = { ...config.params, icode: 'E7B7252EEF983612' }
   // 其他请求，添加到 body 中
   // 如果是上传文件，添加到 FormData 中
   if (config.data instanceof FormData) {
-    config.data.append('icode', '24203399705CE782')
+    config.data.append('icode', 'E7B7252EEF983612')
   } else {
   // 普通的 body 对象，添加到 data 中
-    config.data = { ...config.data, icode: '24203399705CE782' }
+    config.data = { ...config.data, icode: 'E7B7252EEF983612' }
   }
   store.commit('setLoading', true)
   return config
